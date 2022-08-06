c53671175e4817dcc94a40651382758dbc543c41
jalvess021 <101750061+jalvess021@users.noreply.github.com>
2022-08-06 16:30:15 -0300

    Add files via upload

---
+++ play-content/play1.php
@@ -0,0 +1,108 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/all-play.css">
+    <title> Conteúdo | Eadev</title>
+</head>
+<body>
+    <div class="d-flex flex-row ">
+        <div class="gradient">
+            <div class="zoom">
+                <a href="?content_alu=conteudo"><button type="button" class="back"><i class="bi bi-arrow-left"></i> Voltar</button></a>
+                <br>
+                <button type="button" class="info-cur">Html</button>
+                <h2 class="title-cur">HiperText Markup Language</h2>
+                <p class="description-cur">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam a augue purus. Morbi vel urna.</p>
+                <p class="person-cur">Com Gustavo Guanabara</p>
+                <div class=" d-flex flex-row ">
+                    <p class="number-cur"><i class='bi bi-layers-fill ' ></i>   3 Módulo</p>
+                    <p class="quant-cur"><i class='bi bi-collection-play-fill '></i>     20 Aulas</p>
+                </div>
+        
+                <hr noshade=”noshade” class="line">
+                <div class=" d-flex flex-row">
+                    <div class="icon-cur">
+                        <i class="bi bi-star icon_star"></i>
+                        <i class="bi bi-star"></i>
+                        <i class="bi bi-star"></i>
+                        <i class="bi bi-star"></i>
+                        <i class="bi bi-star"></i>
+                    </div>
+                    <p class="av-cur">Deixe sua avaliação</p>
+                </div>
+            </div>
+        
+        </div>
+
+        <div class="all-class">
+            <h4 class="title-class1">Seu progresso</h4>
+
+            
+                <div class=" d-flex flex-row procent-class">
+                    <div class=" progress-bar-cur">
+                            <div class="view-class"></div>
+                    </div>
+                    <p class="porcent-class">75%</p>
+                </div>
+            
+
+            <h3 class="content-title-class">Aulas</h3>
+
+            <div class="all-aula">
+                <div class="all-aula1">
+                    <h4 class="title-class2">Módulo 1</h4>
+                    <a class="d-flex flex-row view-lesson" href="?page=play_video">
+                        <i class="bi bi-camera-video cam"></i>
+                        <p class="name-class">O que é o HiperText Markup Language?</p>
+                        <p class="time-class">00:13:04</p>
+                        <span class="play"><i class="bi bi-play-circle "></i></span>
+                    </a>
+                    <a class=" d-flex flex-row view-lesson">
+                        <i class="bi bi-camera-video cam"></i>
+                        <p class="name-class">O que é o HiperText Markup Language?</p>
+                        <p class="time-class">00:13:04</p>
+                        <i class="bi bi-play-circle play"></i>
+                    </a>
+                    <hr class="line-class">
+                    <h4 class="title-class2">Módulo 2</h4>
+                    <a class=" d-flex flex-row view-lesson">
+                        <i class="bi bi-camera-video cam"></i>
+                        <p class="name-class">O que é o HiperText Markup Language?</p>
+                        <p class="time-class">00:13:04</p>
+                        <i class="bi bi-play-circle play"></i>
+                    </a>
+                    <a class=" d-flex flex-row view-lesson">
+                        <i class="bi bi-camera-video cam"></i>
+                        <p class="name-class">O que é o HiperText Markup Language?</p>
+                        <p class="time-class">00:13:04</p>
+                        <i class="bi bi-play-circle play"></i>
+                    </a>
+                    <hr class="line-class">
+
+                    <h4 class="title-class2">Módulo 3</h4>
+                    <a class=" d-flex flex-row view-lesson">
+                        <i class="bi bi-camera-video cam"></i>
+                        <p class="name-class">O que é o HiperText Markup Language?</p>
+                        <p class="time-class">00:13:04</p>
+                        <i class="bi bi-play-circle play"></i>
+                    </a>
+                    <a class=" d-flex flex-row view-lesson">
+                        <i class="bi bi-camera-video cam"></i>
+                        <p class="name-class">O que é o HiperText Markup Language?</p>
+                        <p class="time-class">00:13:04</p>
+                        <i class="bi bi-play-circle play"></i>
+                    </a>
+                    <hr class="line-class">
+                </div>
+            </div>
+
+        </div>
+
+    </div>
+
+</body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+</html>
---
+++ play-content/play2.php
@@ -0,0 +1,164 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/all-play.css">
+    <title> Conteúdo | Eadev</title>
+</head>
+<body>
+    <div class="d-flex flex-row background">
+
+        <div class="col">
+            <div class="div1">
+                <iframe src="https://www.youtube-nocookie.com/embed/DjOSM72cYac?&theme=dark&autohide=2&modestbranding=1&showinfo=0&rel=0&iv_load_policy=3&start=180" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            
+                <div class="d-flex flex-row group-down">
+            
+                    <div class="col">
+                        <div class="d-flex flex-row group-cur">
+                            <p class="sg-cur">Html</p>
+                            <i class="bi bi-chevron-right"></i>
+                            <p class="int-cur">HiperText Markup Language</p>
+                        </div>
+                        <h4 class="name-video">Formatos de vídeo para o seu site</h4>
+                    </div>
+                    <button class="group-button">Concluir</button>
+                </div>
+            </div>
+        </div>
+
+        <div class="div2">
+
+            <h4 class="title-view">HiperText Markup Language</h4>
+
+            <p class="subtitle-view">Com Gustavo Guanabara</p>
+
+            <p class="text-view">TML (Linguagem de Marcação de HiperTexto) é o bloco de construção mais básico da web. Define o significado e a estrutura do conteúdo da web.</p>
+            
+            <hr class="line-view">
+
+            <div class="all-cur-view">
+                <h4 class="subtitle-view2">Módulo 1</h4>
+                <div class=" d-flex flex-row cur-view">
+                    <i class="bi bi-camera-video cam-view"></i>
+                    <p class="name-cur-view">Formatos de vídeo para o seu site</p>
+                    <a href="?page=play_video" class="play-view"><i class="bi bi-play-circle "></i></a>
+                </div>
+
+                <div class=" d-flex flex-row cur-view">
+                    <i class="bi bi-camera-video cam-view"></i>
+                    <p class="name-cur-view">Formatos de vídeo para o seu site</p>
+                    <a href="?page=play_video" class="play-view"><i class="bi bi-play-circle "></i></a>
+                </div>
+
+                <div class=" d-flex flex-row cur-view">
+                    <i class="bi bi-camera-video cam-view"></i>
+                    <p class="name-cur-view">Formatos de vídeo para o seu site</p>
+                    <a href="?page=play_video" class="play-view"><i class="bi bi-play-circle "></i></a>
+                </div>
+
+                <div class=" d-flex flex-row cur-view">
+                    <i class="bi bi-camera-video cam-view"></i>
+                    <p class="name-cur-view">Formatos de vídeo para o seu site</p>
+                    <a href="?page=play_video" class="play-view"><i class="bi bi-play-circle "></i></a>
+                </div>
+
+                <h4 class="subtitle-view2">Módulo 2</h4>
+                <div class=" d-flex flex-row cur-view">
+                    <i class="bi bi-camera-video cam-view"></i>
+                    <p class="name-cur-view">Formatos de vídeo para o seu site</p>
+                    <a href="?page=play_video" class="play-view"><i class="bi bi-play-circle "></i></a>
+                </div>
+
+                <div class=" d-flex flex-row cur-view">
+                    <i class="bi bi-camera-video cam-view"></i>
+                    <p class="name-cur-view">Formatos de vídeo para o seu site</p>
+                    <a href="?page=play_video" class="play-view"><i class="bi bi-play-circle "></i></a>
+                </div>
+
+                <div class=" d-flex flex-row cur-view">
+                    <i class="bi bi-camera-video cam-view"></i>
+                    <p class="name-cur-view">Formatos de vídeo para o seu site</p>
+                    <a href="?page=play_video" class="play-view"><i class="bi bi-play-circle "></i></a>
+                </div>
+
+                <div class=" d-flex flex-row cur-view">
+                    <i class="bi bi-camera-video cam-view"></i>
+                    <p class="name-cur-view">Formatos de vídeo para o seu site</p>
+                    <a href="?page=play_video" class="play-view"><i class="bi bi-play-circle "></i></a>
+                </div>
+
+                <h4 class="subtitle-view2">Módulo 3</h4>
+                <div class=" d-flex flex-row cur-view">
+                    <i class="bi bi-camera-video cam-view"></i>
+                    <p class="name-cur-view">Formatos de vídeo para o seu site</p>
+                    <a href="?page=play_video" class="play-view"><i class="bi bi-play-circle "></i></a>
+                </div>
+
+                <div class=" d-flex flex-row cur-view">
+                    <i class="bi bi-camera-video cam-view"></i>
+                    <p class="name-cur-view">Formatos de vídeo para o seu site</p>
+                    <a href="?page=play_video" class="play-view"><i class="bi bi-play-circle "></i></a>
+                </div>
+
+                <div class=" d-flex flex-row cur-view">
+                    <i class="bi bi-camera-video cam-view"></i>
+                    <p class="name-cur-view">Formatos de vídeo para o seu site</p>
+                    <a href="?page=play_video" class="play-view"><i class="bi bi-play-circle "></i></a>
+                </div>
+
+                <div class=" d-flex flex-row cur-view">
+                    <i class="bi bi-camera-video cam-view"></i>
+                    <p class="name-cur-view">Formatos de vídeo para o seu site</p>
+                    <a href="?page=play_video" class="play-view"><i class="bi bi-play-circle "></i></a>
+                </div>
+            </div>
+
+        </div>
+
+    </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    <!--<div class="pag-alert">
+        <div class="alert_view">
+            <i class="bi bi-exclamation-triangle icon-alert"></i>
+            <p class="text-alert">Atenção!!! Site em construção</p>
+        </div>
+    </div>
+</body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+</html>-->
