25d15be41d84ad72fcf73f870aa438104e2f2e54
joaoalves2151 <joao.alves.estudante@eteot.rio.br>
2022-08-19 21:09:04 -0300

    att 0.2 - 19/08/2022
    - Pequenas alterações na pág. de controle (Painel - ADM) referente à atualização de dados;

--- base/dashboard/usu_content/adm/default_adm.php
+++ base/dashboard/usu_content/adm/default_adm.php
@@ -13,13 +13,22 @@ $rowAdm = mysqli_num_rows($sqlAdm);
 
 <h3 class="content-title">Painel</h3>
 <h6 class="info-con">Última atualização [ <span id="time-control"><?php  echo date('H:i:s | d-m-Y');?></span> ]</h6>
-<h6 class="info-con-min"> Os registros são atualizados a cada 15 segundos! </h6>
+<h6 class="info-con-min"> Os registros são atualizados a cada 60 segundos!</h6>
+<div id="all-count">
+  <p class="info-count"> Atualizando em: <span id="gfTimeUsu">60</span> </p>
+</div>
+
 <hr>
-<h4> Usuários</h4>	
+	
+<div class="d-flex flex-row justify-content-between">
+  <h5>Usuários</h5>
+    <!-- Chama o Formulário para adicionar Cursos -->
+    <a href="?content_adm=lista_aula&add=aula" class="btn btn-sm bt-padrao float-right"><i class="bi bi-file-earmark-bar-graph-fill"></i> Relatório</a>
+</div>
 <div class="control-plataform">
     <div class="row c1-row justify-content-center align-items-center">
-        <div class="col-xl-7 justify-content-center"> 
-          <div id="piechart" style="width: 900px; height: 500px;"></div>
+        <div class="col-xl-7 justify-content-center" id="gl"> 
+          <div id="piechart" style="width: 100%; height: 500px;"></div>
         </div>
         <div class="table-responsive col-xl-5 table-control">
           <div class="all-table-body">
@@ -52,9 +61,16 @@ $rowAdm = mysqli_num_rows($sqlAdm);
 	      </div>
     </div>
     <hr>
-    <h4 id="ds">Certificados</h4>
-    <div class="c2-row">
-    <div id="curve_chart" style="width: 900px; height: 500px"></div>
+    <div class="d-flex flex-row justify-content-between">
+      <h5>Certificados</h5>
+        <!-- Chama o Formulário para adicionar Cursos -->
+        <a href="?content_adm=lista_aula&add=aula" class="btn btn-sm bt-padrao float-right"><i class="bi bi-file-earmark-bar-graph-fill"></i> Relatório</a>
+    </div>
+    <div class="c2-row row">
+      <div class="col-12">
+      <div id="curve_chart" style=" height: 500px"></div>
+      </div>
+    
     </div>
 </div>
 <script
@@ -63,9 +79,23 @@ $rowAdm = mysqli_num_rows($sqlAdm);
   crossorigin="anonymous"></script>
 <script>
 
-    //ATUALIZANDO EM TEMPO REAL 10s
-    setInterval(function update_visitas(){ //função que será carregada
-            $.ajax({url: "base/dashboard/usu_content/adm/painel-control/data.php", // ajax que vai carregar o arquivo PHP
+   // Atualizar os dados a cada 15s recarregando a pág;
+   var counter = 60;
+    window.setInterval(function () {
+        counter--;
+        if (counter >= 0) {
+            span = document.getElementById("gfTimeUsu");
+            span.innerHTML = counter;
+        }
+        if (counter === 0) {
+            counter = 60;
+        }
+
+    }, 1000);
+    
+    //ATUALIZANDO EM TEMPO REAL 30s
+    setInterval(function update_dados(){ //função que será carregada
+           /* $.ajax({url: "base/dashboard/usu_content/adm/painel-control/data.php", // ajax que vai carregar o arquivo PHP
                     cache: false, // remover cache caso haja
                     success: function(resultData){ //se carregar com sucesso, carrega os dados do PHP
                       $('#time-control').html(resultData); //exibe dentro da DIV #numeroUsu a saída do PHP
@@ -87,80 +117,10 @@ $rowAdm = mysqli_num_rows($sqlAdm);
                   cache: false,
                   success: function(result){ 
                     $('#numeroUsuTb').html(result); 
-                 }});  
-
-          //GRAFICO USUARIO
-         /* google.charts.load('current', {'packages':['corechart']});
-          google.charts.setOnLoadCallback(drawChart);
-
-          $.ajax({url: "base/dashboard/usu_content/adm/painel-control/control_both.php", // ajax que vai carregar o arquivo PHP
-                  cache: false, // remover cache caso haja
-                  success: function(result){ //se carregar com sucesso, carrega os dados do PHP
-                  
-                  function drawChart() {
-
-                      var data = google.visualization.arrayToDataTable([
-                        ['Task', 'Hours per Day'],
-                        ['Alunos',   492],
-                        ['Administradores',      40]
-                      ]);
-
-                      var options = {
-                        title: 'Usuários',
-                        is3D: true,
-                        slices: {
-                          0: { color: '#0CB7B7' },
-                          1: { color: '#6b6b6b' }
-                        }
-                      };
-                      var chart = new google.visualization.PieChart(document.getElementById('piechart'));
-                      chart.draw(data, options);
-                      } 
-                 }}); */
-          
-
-    },15000); // vai rodar a função a cada 15000 milésimos (15 segundos)
+                 }});  */
+                 window.location = window.location.href;
+    },60000); // vai rodar a função a cada 15000 milésimos (15 segundos)
     
-
-    //TESTE AJAX MESMA PAG.
-    /*
-    $(document).ready(function() {
-    $('img').click(function(){
-      //alert("Alerta chamada pelo click na imagem PHP");
-      var nome = $('#nome').val();
-      var email = $('#email').val();
-
-      var varDados = 'nome=' + nome + '&email=' + email;
-      console.log(varDados);
-
-      $.ajax({
-        type: 'POST',
-        url: 'funcoes.php',
-        data: varDados,
-        success: function() {
-          alert('Alerta Sucesso do Ajax: função PHP chamada com sucesso');
-        }
-      });
-
-    });
-
-
-
-    $(document).on('click', '.delete', function(){
-
-$tr = $(this).closest('tr');
-
-$.ajax({
-	type: "POST",
-	url: "apagar.php",
-	data: {id: $tr.attr('id')}
-	success: function(data){
-		$tr.remove();
-	}
-});
-});
-    
-    */
 </script>
 
 <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
@@ -216,7 +176,6 @@ $.ajax({
           var chart = new google.visualization.PieChart(document.getElementById('piechart'));
           chart.draw(data, options);
           }
-          
 </script>
   
 
@@ -247,4 +206,44 @@ $.ajax({
 
         chart.draw(data, options);
       }
+
+      //TESTE AJAX MESMA PAG.
+    /*
+    $(document).ready(function() {
+    $('img').click(function(){
+      //alert("Alerta chamada pelo click na imagem PHP");
+      var nome = $('#nome').val();
+      var email = $('#email').val();
+
+      var varDados = 'nome=' + nome + '&email=' + email;
+      console.log(varDados);
+
+      $.ajax({
+        type: 'POST',
+        url: 'funcoes.php',
+        data: varDados,
+        success: function() {
+          alert('Alerta Sucesso do Ajax: função PHP chamada com sucesso');
+        }
+      });
+
+    });
+
+
+
+    $(document).on('click', '.delete', function(){
+
+$tr = $(this).closest('tr');
+
+$.ajax({
+	type: "POST",
+	url: "apagar.php",
+	data: {id: $tr.attr('id')}
+	success: function(data){
+		$tr.remove();
+	}
+});
+});
+    
+    */
     </script>
