43a4efbe0166dce8b009935e9c99eb2253ff5f3c
João Alves <jalvess021@gmail.com>
2022-09-27 12:57:54 -0300

    att 2.0 - 27/09/2022
    - Pequenos ajustes no arquivo (Play1);

--- play-content/play1.php
+++ play-content/play1.php
@@ -114,33 +114,36 @@ if (isset($_GET['page']) && $_GET['page'] === 'play_curso') {
                             <div class='all-aula1'> ";
 
                     while ($infoMod = mysqli_fetch_array($sqlMod)){
-                        
-                        echo "<h4 class='title-class2'>".$infoMod['nome_mod']."</h4>";
-                        
-                            
-                            //Selecionando as informações da aula através do módulo
-                            $sqlAula = mysqli_query($con, "SELECT * FROM aula where id_mod = '".$infoMod['id_mod']."';");
-                            while ($infoAula = mysqli_fetch_array($sqlAula)){
+                       $sqlIsset = mysqli_query($con, "SELECT COUNT(id_aula) FROM aula a inner join modulo m ON a.id_mod = m.id_mod AND m.id_mod = ".$infoMod['id_mod'].";");
+                       $resIsset = mysqli_fetch_array($sqlIsset);
+                       
+                                if ($resIsset[0] > 0) {
+                                    echo "<h4 class='title-class2'>".$infoMod['nome_mod']."</h4>";
+            
+                                    //Selecionando as informações da aula através do módulo
+                                    $sqlAula = mysqli_query($con, "SELECT * FROM aula where id_mod = '".$infoMod['id_mod']."';");
+                                    while ($infoAula = mysqli_fetch_array($sqlAula)){
 
-                                // Duração da aula
-                                $start = $infoAula['start_aula'];
-                                $end = $infoAula['end_aula'];
-                                $total = $end - $start;
-                                
-                                echo "
-                                <a class='d-flex flex-row justify-content-between view-lesson' href='?page=play_video&curso=".$infoCur['sigla_curso']."&aula=".$infoAula['id_aula']."'>
-                                    <div class='d-flex flex-row'>
-                                        <i class='bi bi-camera-video cam'></i>
-                                        <p class='name-class'>".$infoAula['tit_aula']."</p>
-                                    </div>
-                                    <div class='d-flex flex-row'>
-                                        <p class='time-class'>00:".gmdate("i", $total).":".gmdate("s", $total)."</p>
-                                        <span class='play'><i class='bi bi-play-circle'></i></span>
-                                    </div>
-                                </a>";
-                            } 
+                                        // Duração da aula
+                                        $start = $infoAula['start_aula'];
+                                        $end = $infoAula['end_aula'];
+                                        $total = $end - $start;
+                                        
+                                        echo "
+                                        <a class='d-flex flex-row justify-content-between view-lesson' href='?page=play_video&curso=".$infoCur['sigla_curso']."&aula=".$infoAula['id_aula']."'>
+                                            <div class='d-flex flex-row'>
+                                                <i class='bi bi-camera-video cam'></i>
+                                                <p class='name-class'>".$infoAula['tit_aula']."</p>
+                                            </div>
+                                            <div class='d-flex flex-row'>
+                                                <p class='time-class'>00:".gmdate("i", $total).":".gmdate("s", $total)."</p>
+                                                <span class='play'><i class='bi bi-play-circle'></i></span>
+                                            </div>
+                                        </a>";
+                                    } 
 
-                                echo "<hr class='line-class'>";
+                                        echo "<hr class='line-class'>";
+                                }
                             }
                             
                             
@@ -157,97 +160,6 @@ if (isset($_GET['page']) && $_GET['page'] === 'play_curso') {
     }
 }
 ?>
-
-   <!-- <div class="d-flex flex-row view-overall">
-        <div class="gradient">
-            <div class="zoom">
-                <a href="?content_alu=conteudo"><button type="button" class="back"><i class="bi bi-arrow-left"></i> Voltar</button></a>
-                <br>
-                <button type="button" class="info-cur">Html</button>
-                <h2 class="title-cur">HiperText Markup Language</h2>
-                <p class="description-cur">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam a augue purus. Morbi vel urna.</p>
-                <p class="person-cur">Com Gustavo Guanabara</p>
-                <div class=" d-flex flex-row ">
-                    <p class="number-cur"><i class='bi bi-layers-fill ' ></i>   3 Módulo</p>
-                    <p class="quant-cur"><i class='bi bi-collection-play-fill '></i>     20 Aulas</p>
-                </div>
-        
-                <hr noshade=”noshade” class="line">
-                <div class=" d-flex flex-row">
-                    <div class="icon-cur">
-                        <i class="bi bi-star icon-star"></i>
-                        <i class="bi bi-star icon-star"></i>
-                        <i class="bi bi-star icon-star"></i>
-                        <i class="bi bi-star icon-star"></i>
-                        <i class="bi bi-star icon-star"></i>
-                    </div>
-                    <p class="av-cur">Deixe sua avaliação</p>
-                </div>
-            </div>
-        </div>
-
-        <div class="all-class">
-            <h4 class="title-class1">Seu progresso</h4>
-                <div class=" d-flex flex-row procent-class">
-                    <div class=" progress-bar-cur">
-                            <div class="view-class"></div>
-                    </div>
-                    <p class="porcent-class">75%</p>
-                </div>
-            <h3 class="content-title-class">Aulas</h3>
-
-            <div class="all-aula">
-                <div class="all-aula1">
-                    <h4 class="title-class2">Módulo 1</h4>
-                    <a class="d-flex flex-row view-lesson" href="?page=play_video">
-                        <i class="bi bi-camera-video cam"></i>
-                        <p class="name-class">O que é o HiperText Markup Language?</p>
-                        <p class="time-class">00:13:04</p>
-                        <span class="play"><i class="bi bi-play-circle "></i></span>
-                    </a>
-                    <a class=" d-flex flex-row view-lesson">
-                        <i class="bi bi-camera-video cam"></i>
-                        <p class="name-class">O que é o HiperText Markup Language?</p>
-                        <p class="time-class">00:13:04</p>
-                        <i class="bi bi-play-circle play"></i>
-                    </a>
-                    <hr class="line-class">
-                    <h4 class="title-class2">Módulo 2</h4>
-                    <a class=" d-flex flex-row view-lesson">
-                        <i class="bi bi-camera-video cam"></i>
-                        <p class="name-class">O que é o HiperText Markup Language?</p>
-                        <p class="time-class">00:13:04</p>
-                        <i class="bi bi-play-circle play"></i>
-                    </a>
-                    <a class=" d-flex flex-row view-lesson">
-                        <i class="bi bi-camera-video cam"></i>
-                        <p class="name-class">O que é o HiperText Markup Language?</p>
-                        <p class="time-class">00:13:04</p>
-                        <i class="bi bi-play-circle play"></i>
-                    </a>
-                    <hr class="line-class">
-
-                    <h4 class="title-class2">Módulo 3</h4>
-                    <a class=" d-flex flex-row view-lesson">
-                        <i class="bi bi-camera-video cam"></i>
-                        <p class="name-class">O que é o HiperText Markup Language?</p>
-                        <p class="time-class">00:13:04</p>
-                        <i class="bi bi-play-circle play"></i>
-                    </a>
-                    <a class=" d-flex flex-row view-lesson">
-                        <i class="bi bi-camera-video cam"></i>
-                        <p class="name-class">O que é o HiperText Markup Language?</p>
-                        <p class="time-class">00:13:04</p>
-                        <i class="bi bi-play-circle play"></i>
-                    </a>
-                    <hr class="line-class">
-                </div>
-            </div>
-
-        </div>
-
-    </div> -->
-
 </body>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
 </html>
