abda0a80d16acfeb06964eafb241f49f5021dfd2
João Alves <jalvess021@gmail.com>
2022-10-19 19:48:02 -0300

    Update rel_usuarios.php

--- relatorios/rel_usuarios.php
+++ relatorios/rel_usuarios.php
@@ -11,94 +11,108 @@
     
     $dados = "
     <!DOCTYPE html>
-    <html lang='en'>
+    <html lang='pt-br'>
     <head>
         <meta charset='UTF-8'>
         <title>Pdf Teste</title>
         <link href='https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css' rel='stylesheet' integrity='sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi' crossorigin='anonymous'>
         <link rel='stylesheet' href='http://localhost/tcc/assets/css/all-rel/info_admin.css'>
     </head>
-    <body class='container'>
-        <header class='bg-dark'>
-            <div class='row align-itens-center justify-content-between rel-header '>
-                <div class='col-1'>
-                    <img class='rel-logo' src='http://localhost/tcc/arquivos/img/logo/logo1.png' alt=''> 
-                </div>
-                <div class='col-11 rel-title'>
-                    <h5>Ensino a Distância para Developers</h5>
-                </div>
-            </div>
-            <hr>
-        </header>
-        <main>
-        <div class='row justify-content-between group-rel'>
-            <div class='col-2 '>
-                <img class='perfil-rel' src='http://localhost/tcc/arquivos/img/agnaldo.jfif' alt='Logo Eadev'>
-            </div>
-            <div class='col-3'>
-                <p class='rel-perg'>Nome completo:</p>
-                <p class='rel-resp'>Mateus de Azevedo Correa de Mello</p>
-            </div>
-            <div class='col-3 '>
-                <p class='rel-perg'>E-mail:</p>
-                <p class='rel-resp'>mateuzinho.mello@gmail.com</p>
-            </div>
-    
-            <div class='col-3 mr-4  '>
-                <p class='rel-perg'>Data de nascimento:</p>
-                <p class='rel-resp'>26/02/2004</p>
-            </div>
-    
-        </div>
-        <div class='row justify-content-around group-rel2'>
-            <div class='col-4 '>
-                <p class='rel-perg'>Telefone:</p>
-                <p class='rel-resp'>(21) 98696-5234</p>
-            </div>
-            <div class='col-3'>
-                <p class='rel-perg'>Sexo:</p>
-                <p class='rel-resp'>Masculino</p>
-            </div>
-            <div class='col-2 cpf-rel'>
-                <p class='rel-perg'>CPF:</p>
-                <p class='rel-resp'>111.111.111-11</p>
-            </div>
-        </div>
-        <div class='row justify-content-between group-rel3'>
-    
+    <body>
+    <header class=''>
+      <div class='d-flex flex-row justify-content-center'>
+        <img class='icon-rel' src='http://localhost/tcc/arquivos/img/logo/logo1.png' alt=''>
+      </div>
+      <hr style='margin-top: 4px;'>
+    </header>
+    <main class='pt-4'>
+      <div class='d-flex justify-content center'>
+      <h1 class='title-rel'>Ficha Técnica</h1>
+      <h1 class='title-rel'>Ficha Técnica</h1>
+      </div>
+      <table>
+        <thead>
+          <tr style='padding: 10px; background-color: rgba(19, 19, 19, 0.712);'>
+              
+                <td class='tdth'><img class='person-admin' src='http://localhost/tcc/arquivos/img/agnaldo.jfif' alt=''></td>
+                <td class='tdth'>
+                    <p class='perg-rel'>Nome Completo:</p>
+                    <p class='resp-rel'>Mateus de Azevedo Correa de Mello</p>
+                </td>
+                <td class='tdth'>
+                    <p class='perg-rel'>Nível:</p>
+                    <p class='resp-rel2'>Administrador</p>
+                </td>
+             
             
-            <div class='col-5'>
-                <p class='rel-perg'>RG:</p>
-                <p class='rel-resp'>11.111.111-1</p>
-            </div>
-            <div class='col-2 cep-rel'>
-                <p class='rel-perg'>CEP:</p>
-                <p class='rel-resp'>21610-330</p>
-            </div>
-            <div class='col-3 uf-rel'>
-                <p class='rel-perg'>UF:</p>
-                <p class='rel-resp'>RJ</p>
-            </div>
-        </div>
-        <div class='row justify-content-between  group-rel3'>
-        
-            <div class='col-3'>
-                <p class='rel-perg'>Cidade:</p>
-                <p class='rel-resp'>Rio de Janeiro</p>
-            </div>
-            <div class='col-2'>
-                <p class='rel-perg'>Bairro:</p>
-                <p class='rel-resp'>Marechal Hermes</p>
-            </div>
-            <div class='col-3'>
-                <p class='rel-perg'>Logradouro:</p>
-                <p class='rel-resp'>Xavier Curado</p>
-            </div>
-            
-        </div>
-        </main>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td style='width: 30px !important;' class='perg-rel2'>Usuário:</td>
+            <td class='perg-rel2'>E-mail:</td>
+            <td class='perg-rel2'>Telefone:</td>
+          </tr>
+          <tr>
+            <td class='resp-rel3'>Teuzin</td>
+            <td class='resp-rel3'>Mateuzinho.mello@gmail.com</td>
+            <td class='resp-rel3'>(21)98696-5234</td>
+          </tr>
+          <tr>
+            <td style='width: 200px;' class='perg-rel2'>Data de Nascimento</td>
+            <td class='perg-rel2'>Sexo:</td>
+            <td class='perg-rel2'>CEP:</td>
+          </tr>
+          <tr>
+            <td class='resp-rel3'>26/02/2004</td>
+            <td class='resp-rel3'>Masculino</td>
+            <td class='resp-rel3'>21610-330</td>
+          </tr>
+          <tr>
+            <td class='perg-rel2'>CPF:</td>
+            <td class='perg-rel2'>RG:</td>
+            <td class='perg-rel2'>UF:</td>
+          </tr>
+          <tr>
+            <td class='resp-rel3'>111.111.111-11</td>
+            <td class='resp-rel3'>11.111.111-1</td>
+            <td class='resp-rel3'>RJ</td>
+          </tr>
+          <tr>
+            <td class='perg-rel2'>Cidade:</td>
+            <td class='perg-rel2'>Bairro:</td>
+            <td class='perg-rel2'>Rua:</td>
+          </tr>
+          <tr>
+            <td class='resp-rel3'>Rio de Janeiro</td>
+            <td class='resp-rel3'>Marechal Hermes</td>
+            <td class='resp-rel3'>Rua Xavier Curado</td>
+          </tr>
+          <tr>
+          <td class='perg-rel2' colspan='3'>Atividades do Administrador:</td>
+          </tr>
+          <tr>
+            <td class='perg-rel2'>Inserção</td>
+            <td class='perg-rel2'>Edição</td>
+            <td class='perg-rel2'>Exclusão</td>
+          </tr>
+          <tr>
+            <td height='60' class='resp-rel3'>&nbsp;</td>
+            <td class='resp-rel3'>&nbsp;</td>
+            <td class='resp-rel3'>&nbsp;</td>
+          </tr>
+        </tbody>
+      </table>
+    </main>
+    <footer class='' style='width: 100%;position: absolute; bottom: 0; left: 0;''>
+      <hr style='margin-bottom: 4px;''>
+      <div class='d-flex flex-row justify-content-between'>
+        <img class='footer-icon' src='http://localhost/tcc/arquivos/img/icone/icone1.png' alt=''>
+        <p class='footer-rel'>Data de Emissão: 19/10/2022</p>
+      </div>
+    </footer>
     </body>
-    </html> ";
+    </html>  ";
 
     //Carregar o html;
     $dompdf->loadHtml($dados);
