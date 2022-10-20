018a986a9343fcf66044d37878e034a2b9db3e0c
Mateus <mateuzinho.mello@gmail.com>
2022-10-20 01:36:12 -0300

    att 5.0 - 19/10/2022
    atualização da ficha técnica

--- relteste.php
+++ relteste.php
@@ -7,93 +7,92 @@
         <link rel='stylesheet' href='assets/css/all-rel/info_admin.css'>
     </head>
     <body>
-    <header class='container-fluid'>
-      <div class='d-flex flex-row justify-content-center'>
-        <img class='icon-rel' src='arquivos/img/logo/logo1.png' alt=''>
-      </div>
-      <hr style='margin-top: 4px;'>
-    </header>
-    <main class='container-fluid pt-4'>
-      <div class='d-flex justify-content center'>
-      <h1 class='title-rel'>Ficha Técnica</h1>
-      </div>
-      <table>
-        <thead>
-          <tr class='container'>
-            <th colspan='3' style='padding: 10px; background-color: rgba(19, 19, 19, 0.712);''>
-              <div class='d-flex flex-row justify-content-around'>
-                <img class='person-admin' src='arquivos/img/agnaldo.jfif' alt=''>
-                <p class='perg-rel'>Nome Completo:</p>
+      <header class='container-fluid'>
+        <div class='d-flex flex-row justify-content-center'>
+          <img class='icon-rel' src='arquivos/img/logo/logo1.png' alt=''>
+        </div>
+        <hr style='margin-top: 4px;'>
+      </header>
+      <main class='container-fluid pt-4'>
+        <div class='d-flex justify-content center'>
+          <h1 class='title-rel'>Ficha Técnica</h1>
+        </div>
+        <table>
+          <tbody>
+            <tr>
+              <td style='border-right:none;'>
+                <img class='person-admin' src='arquivos/img/agnaldo.jfif' alt="">
+              </td>
+              <td >
+                <p class='perg-rel'>Nome:</p>
                 <p class='resp-rel'>Mateus de Azevedo Correa de Mello</p>
+              </td>
+              <td>
                 <p class='perg-rel'>Nível:</p>
-                <p class='resp-rel2'>Administrador</p>
-              </div>
-            </th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td style='width: 30px !important;' class='perg-rel2'>Usuário:</td>
-            <td class='perg-rel2'>E-mail:</td>
-            <td class='perg-rel2'>Telefone:</td>
-          </tr>
-          <tr>
-            <td class='resp-rel3'>Teuzin</td>
-            <td class='resp-rel3'>Mateuzinho.mello@gmail.com</td>
-            <td class='resp-rel3'>(21)98696-5234</td>
-          </tr>
-          <tr>
-            <td style='width: 200px;' class='perg-rel2'>Data de Nascimento</td>
-            <td class='perg-rel2'>Sexo:</td>
-            <td class='perg-rel2'>CEP:</td>
-          </tr>
-          <tr>
-            <td class='resp-rel3'>26/02/2004</td>
-            <td class='resp-rel3'>Masculino</td>
-            <td class='resp-rel3'>21610-330</td>
-          </tr>
-          <tr>
-            <td class='perg-rel2'>CPF:</td>
-            <td class='perg-rel2'>RG:</td>
-            <td class='perg-rel2'>UF:</td>
-          </tr>
-          <tr>
-            <td class='resp-rel3'>111.111.111-11</td>
-            <td class='resp-rel3'>11.111.111-1</td>
-            <td class='resp-rel3'>RJ</td>
-          </tr>
-          <tr>
-            <td class='perg-rel2'>Cidade:</td>
-            <td class='perg-rel2'>Bairro:</td>
-            <td class='perg-rel2'>Rua:</td>
-          </tr>
-          <tr>
-            <td class='resp-rel3'>Rio de Janeiro</td>
-            <td class='resp-rel3'>Marechal Hermes</td>
-            <td class='resp-rel3'>Rua Xavier Curado</td>
-          </tr>
-          <tr>
-          <td class='perg-rel2' colspan='3'>Atividades do Administrador:</td>
-          </tr>
-          <tr>
-            <td class='perg-rel2'>Inserção</td>
-            <td class='perg-rel2'>Edição</td>
-            <td class='perg-rel2'>Exclusão</td>
-          </tr>
-          <tr>
-            <td height='60' class='resp-rel3'>&nbsp;</td>
-            <td class='resp-rel3'>&nbsp;</td>
-            <td class='resp-rel3'>&nbsp;</td>
-          </tr>
-        </tbody>
-      </table>
-    </main>
-    <footer class='container-fluid' style='width: 100%;position: absolute; bottom: 0; left: 0;''>
-      <hr style='margin-bottom: 4px;''>
-      <div class='d-flex flex-row justify-content-between'>
-        <img class='footer-icon' src='arquivos/img/icone/icone1.png' alt=''>
-        <p class='footer-rel'>Data de Emissão: 19/10/2022</p>
-      </div>
-    </footer>
+                <p class='resp-rel'>Administrador</p>
+              </td>
+            </tr>
+            <tr>
+              <td class='perg-rel2' style='border: 1px solid black;'>Usuário:</td>
+              <td class='perg-rel2' style='border: 1px solid black ;'>Sexo:</td>
+              <td class='perg-rel2' style='border: 1px solid black ;'>Telefone:</td>
+            </tr>
+            <tr>
+              <td class='resp-rel2' style='border: 1px solid black;'>Teuzin</td>
+              <td class='resp-rel2' style='border: 1px solid black ;'>Masculino</td>
+              <td class='resp-rel2' style='border: 1px solid black ;'>(21) 98696-5234</td>
+            </tr>
+            <tr>
+              <td colspan="2" class='perg-rel2' style='border: 1px solid black;'>E-mail:</td>
+              <td class='perg-rel2' style='border: 1px solid black;'>CPF:</td>
+            </tr>
+            <tr>
+              <td colspan="2" class='resp-rel2' style='border: 1px solid black;'>mateuzinho.mello@gmail.com</td>
+              <td class='resp-rel2' style='border: 1px solid black ;'>111.111.111-11</td>
+            </tr>
+            <tr>
+              <td class='perg-rel2' style='border: 1px solid black;'>RG:</td>
+              <td class='perg-rel2' style='border: 1px solid black ;'>CEP:</td>
+              <td class='perg-rel2' style='border: 1px solid black ;'>UF:</td>
+            </tr>
+            <tr>
+              <td class='resp-rel2' style='border: 1px solid black;'>11.111.111-1</td>
+              <td class='resp-rel2' style='border: 1px solid black ;'>21610-330</td>
+              <td class='resp-rel2' style='border: 1px solid black ;'>RJ</td>
+            </tr>
+            <tr>
+              <td class='perg-rel2' style="border: 1px solid black;">Cidade:</td>
+              <td class='perg-rel2' style="border: 1px solid black ;">Bairro:</td>
+              <td class='perg-rel2' style="border: 1px solid black ;">Rua:</td>
+            </tr>
+            <tr>
+              <td class='resp-rel2' style='border: 1px solid black;'>Rio de Janeiro</td>
+              <td class='resp-rel2' style='border: 1px solid black ;'>Marechal Hermes</td>
+              <td class='resp-rel2' style='border: 1px solid black ;'>Rua Xavier Curado</td>
+            </tr>
+            <tr>
+              <td class='perg-rel2' style='border: 1px solid black;' colspan='3'>Atividades do Administrador:</td>
+              </tr>
+              <tr>
+                <td class='perg-rel2' style='border-right: 1px solid black;'>Inserção</td>
+                <td class='perg-rel2' style='border-right: 1px solid black;'>Edição</td>
+                <td class='perg-rel2' style='border-right: 1px solid black;'>Exclusão</td>
+              </tr>
+              <tr>
+                <td style='border-right: 1px solid black;' height='60' class='resp-rel3'>&nbsp;</td>
+                <td style='border-right: 1px solid black;' class='resp-rel3'>&nbsp;</td>
+                <td style='border-right: 1px solid black;' class='resp-rel3'>&nbsp;</td>
+              </tr>
+          </tbody>
+        </table>
+      
+      </main>
+      <footer class='container-fluid' style='width: 100%;position: absolute; bottom: 0; left: 0;''>
+        <hr style='margin-bottom: 4px;''>
+        <div class='d-flex flex-row justify-content-between'>
+          <img class='footer-icon' src='arquivos/img/icone/icone1.png' alt=''>
+          <p class='footer-rel'>Data de Emissão: 19/10/2022</p>
+        </div>
+      </footer>
     </body>
     </html> 
