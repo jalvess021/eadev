103c9bb20584c5157514c8f47e9338d31bb62175
João Alves <jalvess021@gmail.com>
2022-10-20 23:15:50 -0300

    Update ficha_adm.php

--- relatorios/ficha_adm.php
+++ relatorios/ficha_adm.php
@@ -95,7 +95,7 @@
     <html lang='pt-br'>
     <head>
         <meta charset='UTF-8'>
-        <title>Pdf Teste</title>
+        <title>Ficha Técnica Administrativa</title>
         <link href='https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css' rel='stylesheet' integrity='sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi' crossorigin='anonymous'>
         <link rel='stylesheet' href='http://localhost/tcc/assets/css/all-rel/info_admin.css'>
     </head>
