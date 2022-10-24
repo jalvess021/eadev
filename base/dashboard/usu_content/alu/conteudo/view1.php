521b7b7bc8671c24f19fd131d644a8990d5d02b0
João Alves <jalvess021@gmail.com>
2022-10-24 19:24:34 -0300

    att 1.0 - 24/10/2022
    Mudanças na área de conteúdo do aluno (Visualização dos cursos);

--- base/dashboard/usu_content/alu/conteudo/view1.php
+++ base/dashboard/usu_content/alu/conteudo/view1.php
@@ -1,134 +1,137 @@
 <?php
 
-    $nivel_necessario = 2;
-    include "base/testa_nivel.php";
+      $nivel_necessario = 2;
+      include "base/testa_nivel.php";
 
-    $sql1 = "SELECT * FROM formacao order by id_formacao;";
-    $res = mysqli_query($con, $sql1);
+      $sql1 = "SELECT * FROM formacao order by id_formacao;";
+      $res1 = mysqli_query($con, $sql1);
+      $sql2 = "SELECT * from curso order by id_curso;";      
+      $res2 = mysqli_query($con, $sql2);
+
+      $sql3 = "SELECT * FROM formacao order by id_formacao;";
+      $res3 = mysqli_query($con, $sql3);
 
 ?>
+<link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/swiper/swiper-bundle.min.css'/>
+<ul class=" justify-content-center nav nav-pills row-form">
+      <li class="nav-item">
+          <a class="nav-link form-cur" href="?content_alu=conteudo">Formações</a>
+      </li>
+      <?php
+          while ($info1 = mysqli_fetch_array($res1)) {
+    echo "<li class='nav-item'>
+              <a class='name-cur' id='pills-home-tab' data-toggle='pill' href='#".$info1['nome_formacao']."' role='tab' aria-controls='pills-home' aria-selected='true'>".$info1['nome_formacao']."</a>
+          </li>";
+          }
+      ?>
+</ul>
 
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.css"/>
-<h3 class="content-title">Conteúdo</h3>
-
-    <div class="container">
-
-            
-            <ul class=" justify-content-center nav nav-pills row-form">
-            <li class="nav-item">
-                <a class="nav-link form-cur" href="?content_alu=conteudo">Formações</a>
-            </li>
+<div class="tab-content" id="pills-tabContent">
+  <!--ATIVO-->
+  <div class='tab-pane fade show active' id='All-form-row' role='tabpanel' aria-labelledby='pills-front-end-tab'>
+    <h4 class='caption-card'>Cursos</h4>
+    <hr>
+        <div class='bodyslider'>  
+          <div class='swiper mySwiper'>
+            <div class='swiper-wrapper'>
             <?php
-                while ($info = mysqli_fetch_array($res)) {
-                    echo "<li class='nav-item'>
-                    <a class='name-cur' id='pills-home-tab' data-toggle='pill' href='#".$info['nome_formacao']."' role='tab' aria-controls='pills-home' aria-selected='true'>".$info['nome_formacao']."</a>
-                </li>
-                
-                ";
-                }
-            ?>
-            </ul>
-                <div class="tab-content swiper mySwiper" id="pills-tabContent">
-                <?php
-                $sql1 = "SELECT * FROM formacao order by id_formacao;";
-                $res = mysqli_query($con, $sql1);
-                
-                 while ($info3 = mysqli_fetch_array($res)) {
-                    echo " <div class='tab-pane fade' id='".$info3['nome_formacao']."' role='tabpanel' aria-labelledby='pills-front-end-tab'>
-                    <h4 class='caption-card'>".$info3['nome_formacao']."</h4>
-                    <hr>
-                        <main class='grid'>";
-                            $sql4 = "SELECT * from curso where id_formacao = ".$info3['id_formacao'].";"; 
-                            $res4 = mysqli_query($con, $sql4);
-                            while ($info4 = mysqli_fetch_array($res4)) {
-                                echo "<article>
-                                <img src='\\tcc/base/crud/admin/cursos/imagens/".md5($info4['id_curso']).".jpeg' alt='Imagem do curso (".$info4['sigla_curso'].")'>
-                                <div class='text'>
-                                    <p>".$info4['nome_curso']."</p>
-                                    <a href='?page=play_curso&curso=".$info4['sigla_curso']."'><button  class='btn-card-content'>".$info4['sigla_curso']."</button></a>
-                                </div>
-                            </article> ";          
-                            }    
-                            echo"
-                        </main>
-                        <hr>
-                    </div>";
-                } 
-                
-                $sqlf = "SELECT * FROM formacao order by id_formacao;";
-                $resf = mysqli_query($con, $sqlf);
-                $infof = mysqli_fetch_array($resf);
-                
-                 
-                    echo " <div class='tab-pane fade show active' id='All-form-row' role='tabpanel' aria-labelledby='pills-front-end-tab'>
-                    <h4 class='caption-card'>Cursos</h4>
-                    <hr>
-                        <main class='grid'>";
-                            $sqlff = "SELECT * from curso;";      
-                            $resff = mysqli_query($con, $sqlff);
-                            while ($infoff = mysqli_fetch_array($resff)) {
-                                echo "<article>
-                                <img src='\\tcc/base/crud/admin/cursos/imagens/".md5($infoff['id_curso']).".jpeg' alt='Imagem do curso (".$infoff['sigla_curso'].")'>
-                                <div class='text'>
-                                    <p>".$infoff['nome_curso']."</p>
-                                    <a href='?page=play_curso&curso=".$infoff['sigla_curso']."'><button  class='btn-card-content'>".$infoff['sigla_curso']."</button></a>
-                                </div>
-                            </article> ";          
-                            }    
-                            echo"
-                        </main>
-                        <hr>
-                    </div>";
-         
-                
-                
-                ?>
-                      <div class="swiper-button-next"></div>
-                        <div class="swiper-button-prev"></div>
-            </div>
-    </div>
-
-    <script>
-       const rightButtons = document.querySelectorAll('.swiper-button-next');
-        const leftButtons = document.querySelectorAll('.swiper-button-prev');
-        const rowScroll = document.querySelectorAll('.grid');
-
-        /* array.forEach(element => {
-        }); */
-
-        for (let n = 0; n < rowScroll.length; n++) {
-            const sc = rowScroll[n];
-
-            for (let r = 0; r < rightButtons.length; r++) {
-                const rightBtn = rightButtons[r];
-                rightBtn.addEventListener("click", function() {
-                    sc.scrollLeft  += 380;
-                })
+            while ($info2 = mysqli_fetch_array($res2)) {
+              $queryAula1 = mysqli_query($con, "SELECT COUNT(a.id_aula) FROM aula a INNER JOIN modulo m ON a.id_mod = m.id_mod INNER JOIN curso c ON m.id_curso = c.id_curso AND c.id_curso = ".$info2['id_curso'].";");
+              $infoAula1 = mysqli_fetch_array($queryAula1);
+              echo "
+              <div class='swiper-slide'>
+              <h4>".$info2['sigla_curso']."</h4>
+                <a href='?page=play_curso&curso=".$info2['sigla_curso']."'>
+                  <span class='num-aula'>".$infoAula1[0]." aulas </span>
+                  <div class='bar-cur1'></div>
+                  <div class='bar-cur2'></div>
+                  <div class='opt-box1'></div>
+                  <div class='opt-box2'></div>
+                  <div class='opt-box3'></div>
+                  <div class='ball-cur1'></div>
+                  <div class='ball-cur2'></div>
+                  <img src='\\tcc/base/crud/admin/cursos/imagens/".md5($info2['id_curso']).".jpeg' alt='Curso ".$info2['sigla_curso'].")'>
+                </a>
+                <p>".$info2['nome_curso']."</p>
+              </div>
+              ";
             }
+            ?>
+            </div>
+            <div class='swiper-button-next'></div>
+            <div class='swiper-button-prev'></div>
+            <div class='swiper-pagination'></div>
+          </div>
+        </div>
+    <hr>
+  </div>
 
-            for (let l = 0; l < leftButtons.length; l++) {
-                const leftBtn = leftButtons[l];
-                leftBtn.addEventListener("click", function(){
-                    sc.scrollLeft -= 380;
-                });
-            }            
-        }
-        /*
-        let index = 0;
-        for (const rightButton of rightButtons) {
-            const container = containers[index];
-            rightButton.addEventListener("click", function () {
-                container.scrollLeft += 380;
-            });
-            index++;
-        }
+  <!--Alter-->
+  <?php 
+    while ($info3 = mysqli_fetch_array($res3)) {
+  echo "
+  <div class='tab-pane fade' id='".$info3['nome_formacao']."' role='tabpanel' aria-labelledby='pills-front-end-tab'>
+    <h4 class='caption-card'>".$info3['nome_formacao']."</h4>
+    <hr>
+        <div class='bodyslider'>  
+          <div class='swiper mySwiper'>
+            <div class='swiper-wrapper'>";
+              $sql4 = "SELECT * from curso where id_formacao = ".$info3['id_formacao'].";"; 
+              $res4 = mysqli_query($con, $sql4);
+              while ($info4 = mysqli_fetch_array($res4)) {
+                $queryAula2 = mysqli_query($con, "SELECT COUNT(a.id_aula) FROM aula a INNER JOIN modulo m ON a.id_mod = m.id_mod INNER JOIN curso c ON m.id_curso = c.id_curso AND c.id_curso = ".$info4['id_curso'].";");
+                $infoAula2 = mysqli_fetch_array($queryAula2);
+              echo "
+              <div class='swiper-slide'>
+                <h4>".$info4['sigla_curso']."</h4>
+                <a href='?page=play_curso&curso=".$info4['sigla_curso']."'>
+                  <span class='num-aula'>".$infoAula2[0]." aulas </span>
+                  <div class='bar-cur1'></div>
+                  <div class='bar-cur2'></div>
+                  <div class='opt-box1'></div>
+                  <div class='opt-box2'></div>
+                  <div class='opt-box3'></div>
+                  <div class='ball-cur1'></div>
+                  <div class='ball-cur2'></div>
+                  <img src='\\tcc/base/crud/admin/cursos/imagens/".md5($info4['id_curso']).".jpeg' alt='Imagem do curso (".$info4['sigla_curso'].")'>
+                </a>
+                <p>".$info4['nome_curso']."</p>
+              </div>
+              ";
+              }
+              
+      echo"
+            </div>
+            <div class='swiper-button-next'></div>
+            <div class='swiper-button-prev'></div>
+            <div class='swiper-pagination'></div>
+          </div>
+        </div>
+    <hr>
+  </div>
+  ";
+    }
+  ?>
+</div>
 
-        index = 0;
-        for (const leftButton of leftButtons) {
-            const container = containers[index];
-            leftButton.addEventListener("click", function () {
-                container.scrollLeft -= 380;
-            });
-            index++;
-        } */
-    </script>
+ 
+    
+
+<!-- Swiper JS -->
+<script src='https://cdn.jsdelivr.net/npm/swiper/swiper-bundle.min.js'></script>
+
+<!-- Initialize Swiper -->
+<script>
+  var swiper = new Swiper('.mySwiper', {
+    slidesPerView: 2,
+    spaceBetween: 30,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    }
+  });
+</script>
