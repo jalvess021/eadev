9d5185f1b5789ce7d066833a0a4e28e8f7ec0909
Mateus <mateuzinho.mello@gmail.com>
2022-11-22 13:40:23 -0300

    att 1.0 - 22/11/2022

--- base/dashboard/usu_content/alu/emissao/emissao.php
+++ base/dashboard/usu_content/alu/emissao/emissao.php
@@ -1,16 +1,108 @@
-    <h3 class="content-title">Emissão</h3>
-
-    <div class="container-cert">
-        <div class="card-cert">
-            <div class="face face1">
-                <div class="content-cert">
-                    <img src="arquivos/img/ima" alt="">
-                    <h3>design</h3>
+    <div class="group-cert-pgt">
+        <h4 class='caption-card'>Pagos</h4>
+        <div class="container-cert-pgt ">
+            <div class="card-cert-pgt">
+                <h3> Certificado de HTML</h3>
+                <div class="face face1">
+                    <div class="content-cert-pgt">
+                        <img src="arquivos/img/cert.png" alt="">
+                    </div>
+                </div>
+                <div class="face face2">
+                    <div class="content-cert-pgt">
+                        <p class="subtitle-cert1-pgt">Certificado do curso <b style="font-size: 9.5pt;">HYPERTEXT MARKUP LANGUAGE | HTML</b> já foi pago.</p>
+                        <p class="subtitle-cert2-pgt"><i>Para emiti-lo clique no botão abaixo</i></p>
+                        <button class="btn-cert-pgt">Emitir certificado</button>
+                    </div>
                 </div>
             </div>
-            <div class="face face2">
 
+            <div class="card-cert-pgt">
+                <h3> Certificado de CSS</h3>
+                <div class="face face1">
+                    <div class="content-cert-pgt">
+                        <img src="arquivos/img/cert.png" alt="">
+                    </div>
+                </div>
+                <div class="face face2">
+                    <div class="content-cert-pgt">
+                        <p class="subtitle-cert1-pgt">Certificado do curso <b style="font-size: 9.5pt;">CASCADING STYLE SHEETS | CSS</b> já foi pago.</p>
+                        <p class="subtitle-cert2-pgt"><i>Para emiti-lo clique no botão abaixo</i></p>
+                        <button class="btn-cert-pgt">Emitir certificado</button>
+                    </div>
+                </div>
+            </div>
+
+            <div class="card-cert-pgt">
+                <h3> Certificado de Js</h3>
+                <div class="face face1">
+                    <div class="content-cert-pgt">
+                        <img src="arquivos/img/cert.png" alt="">
+                    </div>
+                </div>
+                <div class="face face2">
+                    <div class="content-cert-pgt">
+                        <p class="subtitle-cert1-pgt">Certificado do curso <b style="font-size: 9.5pt;">JAVASCRIPT | JS</b> já foi pago.</p>
+                        <p class="subtitle-cert2-pgt"><i>Para emiti-lo clique no botão abaixo</i></p>
+                        <button class="btn-cert-pgt">Emitir certificado</button>
+                    </div>
+                </div>
             </div>
         </div>
-    </div>
+    </div> 
+
+    <div class="group-cert-pen">
+        <h4 class='caption-card'>Pendentes</h4>
+        <div class="container-cert-pen ">
+            <div class="card-cert-pen">
+                <h3> Certificado de PHP</h3>
+                <div class="face face1">
+                    <div class="content-cert-pen">
+                        <img src="arquivos/img/cert.png" alt="">
+                    </div>
+                </div>
+                <div class="face face2">
+                    <div class="content-cert-pen">
+                        <p class="subtitle-cert1-pen">Certificado do curso <b style="font-size: 9.5pt;">HYPERTEXT PREPROCESSOR | PHP</b> pendente.</p>
+                        <p class="subtitle-cert2-pen"><i>Para realizar pagamento clique no botão abaixo</i></p>
+                        <button class="btn-cert-pen">Realizar Pagamento</button>
+                    </div>
+                </div>
+            </div>
+
+            <div class="card-cert-pen">
+                <h3> Certificado de SQL</h3>
+                <div class="face face1">
+                    <div class="content-cert-pen">
+                        <img src="arquivos/img/cert.png" alt="">
+                    </div>
+                </div>
+                <div class="face face2">
+                    <div class="content-cert-pen">
+                        <p class="subtitle-cert1-pen">Certificado do curso <b style="font-size: 9.5pt;">STRUCTURED QUERY LANGUAGE | SQL</b> pendente.</p>
+                        <p class="subtitle-cert2-pen"><i>Para realizar pagamento clique no botão abaixo</i></p>
+                        <button class="btn-cert-pen">Realizar Pagamento</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div> 
+
+
+    <div class="group-cert-bloq">
+        <h4 class='caption-card'>Bloqueados</h4>
+        <div class="container-cert-bloq">
+            <div class="card-cert-bloq">
+                <h3> Certificado de GitHub</h3>
+                <div class="face face1">
+                    <div class="content-cert-bloq">
+                        <img src="arquivos/img/cert.png" alt="">
+                        <i class="bi bi-lock-fill"></i>
+                        <h6>Bloqueado</h6>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div> 
+
 
