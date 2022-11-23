f2e6344c7afc41d582da1c742943fc4f3b8d6f60
Mateus <mateuzinho.mello@gmail.com>
2022-11-23 02:17:12 -0300

    att 4.0 - 22/11/2022

--- base/dashboard/usu_content/alu/pagamento/pagamento.php
+++ base/dashboard/usu_content/alu/pagamento/pagamento.php
@@ -1,1 +1,129 @@
-<h3 class="content-title">Finalizar Pagamento</h3>
+
+<div class="row">
+    <h3 class="content-title col-9 m-0">Área de Pagamento</h3>
+    <div class="col-3"><a href="?content_adm=view" class="float-right btn-back btn btn-sm bt-padrao"> <i class="bi bi-arrow-left"></i> Voltar </a></div>
+</div>
+
+
+    <div class="all-pgt ">
+        <div class="group-data-end ">
+            <div class="personal-data-pgt mb-4">
+                <h3 class="title-personal-data"><i class="bi bi-person-fill"></i>Dados do Comprador</h3>
+                <div class="d-flex flex-column ml-3 mt-2">
+                        <div class="d-flex flex-row">
+                            <h6 class="perg-personal-data">Nome:</h6>
+                            <h6 class="resp-personal-data">Mateus de Azevedo Correa de Mello</h6>
+                        </div>
+                        <div class="d-flex flex-row">
+                            <h6 class="perg-personal-data">Email:</h6>
+                            <h6 class="resp-personal-data">mateuzinho.mello@gmail.com</h6>
+                        </div>
+                        <div class="d-flex flex-row">
+                            <h6 class="perg-personal-data">CPF:</h6>
+                            <h6 class="resp-personal-data">161.670.067-09</h6>
+                        </div>
+                </div>
+            </div>
+            <div class="personal-data-end ">
+                <h3 class="title-personal-end"><i class="bi bi-house-fill"></i>Endereço</h3>
+                <div class=" group-end d-flex flex-row">
+                    <div class="circle-loc-end">
+                    <i class="bi bi-geo-alt-fill loc-end"></i>
+                    </div>
+                    <div class="d-flex flex-column ml-2 mt-3">
+                        <h6 class="resp-end"> Xavier Curado, (21) 33333-3333</h6>
+                        <h6 class="resp-end">Rio de Janeiro - RJ - Marechal Hermes</h6>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="group-rec-cert">
+            <h3 class="title-personal-cert"></i>Certificado de HTML</h3>
+            <div class="d-flex flex-row mt-5">
+                <div class="group-img">
+                    <img src="arquivos/img/cert.png" alt="">
+                </div>
+                <div class="group-pricing">
+                    <div class="d-flex flex-row justify-content-between mb-2">
+                        <h6 class="subtotal-cert">Produto:</h6>
+                        <h6 class="subtotal-cert-pricing">Certificado</h6>
+                    </div>
+                    <div class="d-flex flex-row justify-content-between mb-2">
+                        <h6 class="subtotal-cert">Subtotal:</h6>
+                        <h6 class="subtotal-cert-pricing">R$ 59,99</h6>
+                    </div>
+                    <div class="d-flex flex-row justify-content-between ">
+                        <h6 class="subtotal-cert">Desconto:</h6>
+                        <h6 class="subtotal-cert-pricing">R$ 20,00</h6>
+                    </div>
+                    <hr class="line-pgt">
+                    <div class="d-flex flex-row justify-content-between ">
+                        <h6 class="total-cert">Total:</h6>
+                        <h6 class="total-cert-pricing ">R$ 39,99</h6>
+                    </div>
+                </div>
+            </div>
+            <div class="d-flex justify-content-center mt-5">
+                <a data-toggle="modal" data-target="#exampleModal"><button class="btn-pgt">Finalizar Pagamento</button></a>
+            </div>
+        </div>
+    </div>
+
+    <!-- Modal -->
+    <div class="modal fade " id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered " role="document">
+            <div class="modal-content ">
+            <div class="modal-header header-pgt">
+        
+            </div>
+                <div class="modal-body body-modal-pgt">
+                    <div class="d-flex flex-row justify-content-between mt-3">
+                        <h3 class="title-personal-pgt"></i>Pagamento via Pix</h3>
+                        <button type="button" class="close close-pgt" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class=" all-modal-pgt">
+                        <div class="view-pricing ">
+                            <i class="bi bi-check-circle"></i>
+                            <h6 class="subtotal-pgt">Valor para pagamento:</h6>
+                            <h6 class="subtotal-pgt-pricing">R$ 39,99</h6>
+                        </div>
+                        <div class="d-flex flex-row mt-3">
+                            <div class="group-qrcode">
+                                <img class="qrcode-pix" src="arquivos/img/qrcode.png" alt="">
+                                <button type="button" class=" btn-pix">Código Pix</button>
+                                <img class="pix-img" src="arquivos/img/pix.png" alt="">
+                            </div>
+                            <div class="instruction-pgt">
+                                <h6 class="title-instruction">Instruções</h6>
+                                <div class="d-flex flex-row">
+                                    <p class="description-instruction"><b>1.</b></p>
+                                    <p class="description-instruction2">Abra o Aplicativo do seu banco.</p>
+                                </div>
+                                <div class="d-flex flex-row">
+                                    <p class="description-instruction"><b>2.</b></p>
+                                    <p class="description-instruction2">Escolha a opção Pix e cole o código ou use a câmera do celular para ler o QR Code.</p>
+                                </div>
+                                <div class="d-flex flex-row">
+                                    <p class="description-instruction"><b>3.</b></p>
+                                    <p class="description-instruction2">Confirme as informações e finalize o pagamento.</p>
+                                </div>
+                                <div class="d-flex flex-row">
+                                    <p class="description-instruction"><b>4.</b></p>
+                                    <p class="description-instruction2">Caso tenha efetuado o pagamento, clique no botão Paguei com Pix e aguarde alguns minutos até a confirmação do pagamento.</p>
+                                </div>
+                                
+                                <button type="button" class=" btn-pix-pgt">Paguei com Pix</button>
+                            </div>
+
+                        </div>
+                    </div> 
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    
