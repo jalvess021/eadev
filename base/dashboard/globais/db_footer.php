760e2251d14c6c062a0c9cd8b343c1e62b371949
jalvess021 <jalvess021@gmail.com>
2024-10-17 17:53:33 -0300

    Fix: Link no footer, creditos de desenvolvimento

--- base/dashboard/globais/db_footer.php
+++ base/dashboard/globais/db_footer.php
@@ -4,9 +4,9 @@
 ?>
 <footer class="footer bg-white">
 	<span class="text-right text-dark">
-		Copyright &copy; <a target="_blank" href="#" style="color: #0d9999;">Eadev/2022</a>
+		Copyright &copy; <a target="_blank" href="https://github.com/jalvess021/eadev" style="color: #0d9999;">Eadev/2022</a>
 	</span>
 	<span class="float-right text-dark">
-		Produzido por <a target="_blank" href="#"><b style="color: #0d9999;">Tecnodev's</b></a>
+		Produzido por <a target="_blank" href="https://github.com/jalvess021"><b style="color: #0d9999;">@jalvess021</b></a>
 	</span>
 </footer>
